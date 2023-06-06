a5b2f27490a213749f245ffda4ac83e6fdc715e8
Fredoski26 <fredrickobarafor@yahoo.com>
2023-06-06 18:32:59 +0100

    added some property

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/api/v1/products', async(req, res) =>{
 app.get('/api/v1/products', async(req, res) =>{
     try {
        const products = await Product.find({})
-       res.status(200).json({success: true, data: products, msg: 'products load successfully'})
+       res.status(200).json({success: true, data: products, total_size: 6, type_id: 2, offset:0, msg: 'products load successfully'})
     } catch (error) {
         res.status(500).json({message: error.message}) 
     }
